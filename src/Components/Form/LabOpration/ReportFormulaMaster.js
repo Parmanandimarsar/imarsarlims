7f5efaefa6e39265d38c79b86e3efb186c0d4294
Parmanandimarsar <parmanand.maurya@imarsar.com>
2024-10-29 17:22:18 +0530

    update

---
+++ src/Components/Form/LabOpration/ReportFormulaMaster.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportFormulaMaster = () => {
+  return (
+    <div>ReportFormulaMaster</div>
+  )
+}
+
+export default ReportFormulaMaster
--- src/Components/Form/LabOpration/ReportingHelpMaster/ReportingHelpModal.js
+++ src/Components/Form/LabOpration/ReportingHelpMaster/ReportingHelpModal.js
@@ -1,48 +1,59 @@
-import React from 'react'
-import { Box, Button, Modal, TextField, Typography } from '@mui/material'
+import React from "react";
+import {
+  Box,
+  Button,
+  FormLabel,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { RxCross2 } from "react-icons/rx";
 
-const ReportingHelpModal = ({modalOpen,
-    handleCloseModal,
-    handleSaveMasterName,
-    isEdit,
-    newMasterName,
-    setNewMasterName,}) => {
+const ReportingHelpModal = ({
+  modalOpen,
+  handleCloseModal,
+  handleSaveMasterName,
+  isEdit,
+  newMasterName,
+  setNewMasterName,
+}) => {
   return (
     <Modal open={modalOpen} onClose={handleCloseModal}>
-    <Box
-      sx={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: 400,
-        bgcolor: "background.paper",
-        boxShadow: 24,
-        p: 4,
-        borderRadius: "8px",
-      }}
-    >
-      <Typography variant="h6" mb={2}>
-        {isEdit  ? "Edit Help" : "Add New Help"}
-      </Typography>
-      <TextField
-        fullWidth
-        label="Master Name"
-        value={newMasterName}
-        onChange={(e) => setNewMasterName(e.target.value)}
-        sx={{ mb: 2 }}
-      />
-      <Button
-        variant="contained"
-        onClick={handleSaveMasterName}
-        fullWidth
-        disabled={!newMasterName.trim()}
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          width: 300,
+          bgcolor: "background.paper",
+          boxShadow: 24,
+          p: 2,
+          borderRadius: "8px",
+        }}
       >
-        Save
-      </Button>
-    </Box>
-  </Modal>
-  )
-}
+        <Typography variant="h6"  sx={{ fontWeight: "bold" }}>
+          {isEdit ? "Edit Help" : "Add New Help"}
+        </Typography>
+        <div className=" absolute  right-2 top-2  " onClick={handleCloseModal}>
+        <RxCross2 className="text-gray-500 text-2xl" />
+      </div>
+        <FormLabel>Master Name</FormLabel>
+        <TextField
+          fullWidth
+          value={newMasterName}
+          onChange={(e) => setNewMasterName(e.target.value)}
+          sx={{ mb: 2 }}
+        />
+        <button
+          className="border-none project-thim text-white rounded-md "
+          disabled={!newMasterName.trim()}
+        >
+          Save
+        </button>
+      </Box>
+    </Modal>
+  );
+};
 
-export default ReportingHelpModal
+export default ReportingHelpModal;
